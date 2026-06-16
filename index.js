578bd0557dc8f911cb90a0e5e187cb73ad97f2dc
santu-santu8 <nayanasantosh.23.cse@anits.edu.in>
2026-06-16 10:12:26 +0530

    Fix backend URL

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://realtime-chat-backend-5kgp.onrender.com",
     methods: ["GET", "POST"],
   },
 });
