3d7646a13d25768f7b94b136ef0c2bdac79e0761
santu-santu8 <nayanasantosh.23.cse@anits.edu.in>
2026-06-16 11:29:56 +0530

    Fix users route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,46 +1,25 @@
-const express =
-  require("express");
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
 
-const router =
-  express.Router();
+// GET ALL USERS
+router.get("/", async (req, res) => {
+  try {
+    const users = await db.query(`
+      SELECT id, username, email
+      FROM users
+      ORDER BY username
+    `);
 
-const db =
-  require("../config/db");
+    res.json(users.rows);
 
-// GET USERS
-router.get(
-  "/",
-  async (req, res) => {
+  } catch (error) {
+    console.log("Users Error:", error);
 
-    try {
-
-      const users =
-        await db.query(
-          `
-          SELECT
-          id,
-          username,
-          email,
-          last_seen
-          FROM users
-          `
-        );
-
-      res.json(
-        users.rows
-      );
-
-    } catch (error) {
-
-      console.log(error);
-
-      res.status(500).json({
-        message:
-          "Server Error",
-      });
-    }
+    res.status(500).json({
+      message: "Server Error",
+    });
   }
-);
+});
 
-module.exports =
-  router;
+module.exports = router;
